af740c0b8adff66889b4fd6adeb719a85ad98b9b
Rafael <fafagm99@gmail.com>
2025-05-12 00:10:50 -0600

    added buttons to profile forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 import { DateHandler } from "./DateHandler.js";
 import { PDFGenerator } from "./PDFGenerator.js";
+import { GeneralUtils } from "./GeneralUtils.js";
 
 //Initialize date handling
 const MyDateHandler = new DateHandler()
