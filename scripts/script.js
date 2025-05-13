476cf93a07aa774d7c336e699a9beddd92fbae66
Rafael <fafagm99@gmail.com>
2025-05-13 12:28:08 -0600

    Init commit it

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-import { DateHandler } from "./DateHandler.js";
-import { PDFGenerator } from "./PDFGenerator.js";
-import { GeneralUtils } from "./GeneralUtils.js";
-
-//Initialize date handling
-const MyDateHandler = new DateHandler()
-MyDateHandler.init();
-
